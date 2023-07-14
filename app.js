28576fe948f27c108686756043583bc30940653d
David Munyiri <davidmumyiri2019@gmail.com>
2023-07-14 11:34:42 +0300

    final commit today

--- app.js
+++ app.js
@@ -47,3 +47,14 @@ let appendValue=(value)=> {
     expression += value;
     document.getElementById('display').value = expression;
   }
+
+  function calculate() {
+    try {
+      const result = eval(expression);
+      document.getElementById('display').value = result;
+      expression = '';
+    } catch (error) {
+      document.getElementById('display').value = 'Error';
+      expression = '';
+    }
+  }
